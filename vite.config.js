fe2d702c471a3a88bba4a65b69da1411e2651f61
蒲强 <pu.qiang@qq.com>
2026-01-12 16:30:55 +0800

    chore:配置调整

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    build: {
+        chunkSizeWarningLimit: 1000, // 可以适当调大，配合分包使用
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    // 将 node_modules 中的依赖拆分到 vendor chunk 中
+                    if (id.includes('node_modules')) {
+                        return 'vendor';
+                    }
+                }
+            }
+        }
+    },
 })
