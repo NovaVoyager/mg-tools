0e1646dd24968fc5ab2530ee635135b8af913f59
蒲强 <pu.qiang@qq.com>
2026-01-13 11:33:13 +0800

    chore:add sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,26 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        Sitemap({
+            hostname: 'https://tools.37pq.cn',
+            dynamicRoutes: [
+                '/',
+                '/#/image-format',
+                '/#/json-tools',
+                '/#/timestamp',
+                '/#/base64',
+                '/#/url',
+            ],
+            changefreq: 'weekly',
+            priority: 0.8,
+            lastmod: new Date().toISOString().split('T')[0],
+        })
+    ],
     build: {
         chunkSizeWarningLimit: 1000, // 可以适当调大，配合分包使用
         rollupOptions: {
